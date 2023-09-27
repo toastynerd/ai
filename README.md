415bed740169af41245f247ecf5f76aa60071d4e
Tyler Morgan <tylermorgan86@gmail.com>
2023-09-27 09:23:39 -0700

    Outline for adding a propmpt

---
+++ README.md
@@ -0,0 +1,5 @@
+# AI cli
+![Status](https://github.com/toastynerd/ai/workflows/ai/badge.svg)
+Making an ai cli for myself. For now I want to be able to save a prompt and replace
+variabls within that prompt and run it against ChatGPT.
+
---
+++ src/main.rs
@@ -0,0 +1,30 @@
+use std::path::PathBuf;
+use clap::{arg, Command};
+
+fn cli() -> Command {
+    Command::new("prompt")
+        .about("Interface with chatGPT prompts")
+        .subcommand_required(true)
+        .arg_required_else_help(true)
+        .allow_external_subcommands(false)
+        .subcommand(
+            Command::new("add")
+                .about("Add a prompt")
+                .arg(arg!(<NAME> "the name of the prompt"))
+                .arg(arg!(<PROMPT> "the prompt template"))
+        )
+
+}
+
+fn main() {
+    let matches = cli().get_matches();
+    match matches.subcommand() {
+        Some(("add", sub_matches)) => {
+            let name = sub_matches.get_one::<String>("NAME").expect("required");
+            let prompt = sub_matches.get_one::<String>("PROMPT").expect("required");
+            let num_params = prompt.matches(|c| c == '[').count();
+            println!("Added prompt \"{}\" with template \"{}\" with {num_params} parameters", name, prompt);
+        }
+        _ => unreachable!(),
+    }
+}
